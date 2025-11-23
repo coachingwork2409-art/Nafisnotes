d295d15a334e1d0f5e2c86ce6c8330d21503fde0
coachingwork2409-art <coachingwork2409@gmail.com>
2025-11-23 16:50:12 +0530

    add script.js

---
+++ css/js/script.js
@@ -0,0 +1,9 @@
+// OPEN SIDE MENU
+function openMenu() {
+  document.getElementById("sideMenu").style.right = "0";
+}
+
+// CLOSE SIDE MENU
+function closeMenu() {
+  document.getElementById("sideMenu").style.right = "-260px";
+}
