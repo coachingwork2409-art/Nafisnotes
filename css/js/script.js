7d8fe858863a8148a77feefd44bf7ae1884e0650
coachingwork2409-art <coachingwork2409@gmail.com>
2025-11-23 16:51:56 +0530

    Delete css/js directory

--- css/js/script.js
+++
@@ -1,9 +0,0 @@
-// OPEN SIDE MENU
-function openMenu() {
-  document.getElementById("sideMenu").style.right = "0";
-}
-
-// CLOSE SIDE MENU
-function closeMenu() {
-  document.getElementById("sideMenu").style.right = "-260px";
-}
