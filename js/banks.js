cdf9e82162fcb720515ed458004e8e1e186a3b44
coachingwork2409-art <coachingwork2409@gmail.com>
2025-11-25 18:31:30 +0530

    Create banks.js

---
+++ js/banks.js
@@ -0,0 +1,13 @@
+// ==== ALL SUBJECT QUESTION BANK ====
+// Class 10 - Bihar Board (Hindi Medium)
+
+const questionBank = {
+  maths: [],
+  science: [],
+  sst: [],
+  english: [],
+  nonhindi: [],
+  urdu: [],
+  maithili: [],
+  sanskrit: []
+};
