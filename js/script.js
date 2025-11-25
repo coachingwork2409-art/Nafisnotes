5eff28f3ad74edbf027707419c6699fb288e02bf
coachingwork2409-art <coachingwork2409@gmail.com>
2025-11-25 21:14:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -187,3 +187,26 @@ if(e.key === 'ArrowLeft') prevBtn.click();
 });
 
 })();
+// ============================
+// SIDEMENU FINAL WORKING CODE
+// ============================
+
+function openMenu(){
+  const menu = document.getElementById("sideMenu");
+  const overlay = document.getElementById("overlay");
+
+  if(menu && overlay){
+    menu.classList.add("open");
+    overlay.classList.add("show");
+  }
+}
+
+function closeMenu(){
+  const menu = document.getElementById("sideMenu");
+  const overlay = document.getElementById("overlay");
+
+  if(menu && overlay){
+    menu.classList.remove("open");
+    overlay.classList.remove("show");
+  }
+}
